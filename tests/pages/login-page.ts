b71efece9b2ff70fe9212f55e428c46f829a4911
jaana <jaana.jegorova@gmail.com>
2025-02-28 19:59:44 +0200

    feat/jj/TL-18

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -3,21 +3,19 @@ import { OrderPage } from './order-page'
 import { SERVICE_URL } from '../../config/env-data'
 import { Input } from '../atoms/Input'
 import { Button } from '../atoms/Button'
+import { BasePage } from './base-page'
 
-export class LoginPage {
-  readonly page: Page
+export class LoginPage extends BasePage {
   readonly url: string = SERVICE_URL
   readonly signInButton: Button
   readonly usernameField: Input
   readonly passwordField: Input
-  // add more locators here
 
   constructor(page: Page) {
-    this.page = page
-    this.signInButton = new Button(page, '[data-name=signIn-button]') //(page, 'button')
+    super(page)
+    this.signInButton = new Button(page, '[data-name=signIn-button]')
     this.usernameField = new Input(page, '[data-name=username-input]')
     this.passwordField = new Input(page, '[data-name=password-input]')
-    // continue with the rest of the implementation below
   }
 
   async open() {
@@ -32,6 +30,4 @@ export class LoginPage {
     await this.signInButton.click()
     return new OrderPage(this.page)
   }
-
-  // continue with the rest of the implementation below
 }
--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,19 +1,28 @@
-import { Page } from '@playwright/test'
+import { Locator, Page } from '@playwright/test'
 import { Input } from '../atoms/Input'
 import { Button } from '../atoms/Button'
+import { BasePage } from './base-page'
 
-export class OrderPage {
-  readonly page: Page
+export class OrderPage extends BasePage {
   readonly statusButton: Button
   readonly nameField: Input
   readonly phoneField: Input
   readonly commentField: Input
+  readonly statusModal: Locator
+  readonly orderNumberFiled: Input
+  readonly trackButton: Button
 
   constructor(page: Page) {
-    this.page = page
+    super(page)
     this.statusButton = new Button(page, '[data-name=openStatusPopup-button]')
     this.nameField = new Input(page, '#name')
     this.phoneField = new Input(page, '#phone')
     this.commentField = new Input(page, '#comment')
+    this.statusModal = page.getByTestId('searchOrder-popup')
+    this.orderNumberFiled = new Input(page, '[data-name="searchOrder-popup"] input')
+    this.trackButton = new Button(
+      page,
+      '[data-name="searchOrder-popup"] button.order-search-popup__button',
+    )
   }
 }
